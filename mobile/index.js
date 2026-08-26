9550495536d2b855dde199bca6077f1add94475b
purna <purna@example.com>
2026-08-26 16:36:38 +0530

    fix(mobile): call enableFreeze(false) in index.js to prevent react-freeze SceneView TypeError

--- mobile/index.js
+++ mobile/index.js
@@ -3,8 +3,11 @@
  */
 
 import 'react-native-gesture-handler';
+import {enableFreeze} from 'react-native-screens';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+
+enableFreeze(false);
 
 AppRegistry.registerComponent(appName, () => App);
